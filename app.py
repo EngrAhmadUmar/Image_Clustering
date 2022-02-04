a21f33f5eeb8a8a89b2e97093796924ca0792efb
Ahmad Ahmed <zcaptain2002@gmail.com>
2022-02-04 02:59:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,6 @@ def main():
     # display the front end aspect
     st.markdown(html_temp, unsafe_allow_html=True)
     default_value_goes_here = ""
-    # ball_control = st.number_input("Please enter the players Ball Control Attribute", 0, 100000000, 0)
-    # short_passing = st.number_input("Please enter the players Short Passing Attribute", 0, 100000000, 0)
-    # dribbling = st.number_input("Please enter the players Dribbling Attribute", 0, 100000000, 0)
-    # crossing = st.number_input("Please enter the players Crossing Attribute", 0, 100000000, 0)
-    # curve = st.number_input("Please enter the players Curve Attribute", 0, 100000000, 0)
 
     uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
 
@@ -36,25 +31,11 @@ def main():
     if uploaded_file:
         df = pd.read_excel(uploaded_file)
         dataframe = df
-        # st.dataframe(df)
-        # st.table(df)
-
-    # attributes = [ball_control, short_passing, dribbling, crossing, curve]
-    #
+   
     result = ""
-    #
-    # # Display Books
     if st.button("Predict"):
-#       arr = dataframe.columns
-
-#       for i in arr:
-#           notnull = dataframe[i][dataframe[i].notnull()]
-#           min = notnull.min()
-#           dataframe[i].replace(np.nan, min, inplace=True)
-
-#       scaler = StandardScaler()
-#       scaler.fit(dataframe)
       featureshost = dataframe
+      model = KMeans(n_clusters=10, max_iter=1000)
       prediction = model.fit(featureshost)
     
       result = prediction.labels_
